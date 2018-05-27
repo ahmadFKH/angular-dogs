e6cf7dabfed7eda115e3fb369b3a0c73235b8d6b
Ahmad Khalilieh <ahmadfk90@gmail.com>
2018-05-27 14:05:04 +0300

    intro to angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'my first angular 2.0 adventure!';
+  dogName:string = 'Rex';
+  dogWeight: number;
+  imageUrl: string;
+  ownerName: string = 'ahmad';
+  constructor() {
+    var d = new Date();
+    var n = d.getHours();
+    if (n >= 7 && n <= 20 ) {
+      this.imageUrl = 'http://imgsrv.wkdzradio.com/image/wkdz4/UserFiles/Image/Dog%20Day1.jpg';
+    }else {
+      this.imageUrl = 'https://besthqwallpapers.com/Uploads/22-1-2018/37943/thumb2-4k-moon-dog-night-digital-art.jpg';
+    }
+  }
 }
